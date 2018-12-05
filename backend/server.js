51e7c9ffedfd70155033a09171aa98f4da61a547
Kevin Griego <kev.gri32@gmail.com>
2018-12-05 14:34:04 -0700

    still adding passport, changed names of models

--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,11 @@ const port = process.env.PORT || 8000;
 const cors = require('cors');
 const logger = require('morgan');
 const knex = require('./db/knex');
+const session = require('express-session');
 
 const index = require('./routes/index');
 const todos = require('./routes/todos');
+const users = require('./routes/users');
 
 const app = express();
 
@@ -17,8 +19,12 @@ app.use(logger('dev'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+//use for passport
+app.use(session({ secret: 'cats' }));
 
+//routes
 app.use('/', index);
+app.use('/users', users);
 app.use('/todos', todos);
 
 app.listen(port, () => {
