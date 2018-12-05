d4ab9a686836516081f51e301af8e7d887546074
Kevin Griego <kev.gri32@gmail.com>
2018-12-05 14:58:29 -0700

    adding passport local to package

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const logger = require('morgan');
 const knex = require('./db/knex');
 const session = require('express-session');
+const passport = require('passport');
 
 const index = require('./routes/index');
 const todos = require('./routes/todos');
@@ -20,8 +21,11 @@ app.use(logger('dev'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//use for passport
-app.use(session({ secret: process.env.secret }));
+
+//middleware use for passport
+app.use(session({ secret: process.env.secret, resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+
 //routes
 app.use('/', index);
 app.use('/users', users);
