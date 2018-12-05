c2e35d27f8c956585cdadc618f31b0c3f1b93657
David Kruse <thedavidkruse@gmail.com>
2018-12-05 13:54:32 -0700

    added favorites

---
+++ backend/db/migrations/20181205135246_favorites.js
@@ -0,0 +1,11 @@
+exports.up = (knex, Promise) => {
+    return knex.schema.createTable('favorites', (table) => {
+      table.foreign("uuid").references("users.id");
+      table.foreign("video_id").references("videos.id");
+
+    });
+  };
+  
+  exports.down = (knex, Promise) => {
+    return knex.schema.dropTable('favorites');
+  };
