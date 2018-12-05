e6c202dc7e3c0de19795cdf7948178052c312dde
David Kruse <thedavidkruse@gmail.com>
2018-12-05 10:46:00 -0700

    initial setup

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 8000;
+const cors = require('cors');
+const logger = require('morgan');
+const knex = require('./db/knex');
+
+const index = require('./routes/indexRoutes');
+const todos = require('./routes/todosRoutes');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(logger('dev'));
+app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', index);
+app.use('/todos', todos);
+
+app.listen(port, function() {
+  console.log("listening on port: ", port);
+})
