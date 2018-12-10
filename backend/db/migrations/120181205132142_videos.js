6b1ac0ef54be7ea824a0d2a82b2567a665fbeb05
Christopher White <christopher.evan.white88@gmail.com>
2018-12-10 14:17:43 -0700

    committing to pull changes

--- backend/db/migrations/120181205132142_videos.js
+++ backend/db/migrations/120181205132142_videos.js
@@ -9,7 +9,6 @@ exports.up = (knex, Promise) => {
     table.foreign('user_uuid').references('users_pkey');
   });
 };
-// alter table "videos" add constraint "videos_user_uuid_foreign" foreign key("user_uuid") references "users"("uuid")
 
 exports.down = (knex, Promise) => {
   return knex.schema.dropTable('videos');
--- backend/routes/playlist.js
+++ backend/routes/playlist.js
@@ -2,98 +2,90 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
 
-const knex = require('./knex'); // the connection!
-
-module.exports = {
-  getAll() {
-    return knex('sticker');
+const dao = {
+  getAll: id => {
+    return knex('playlists').where('user_uuid', id);
   },
-  getOne(id) {
-    return knex('sticker')
-      .where('id', id)
+  getOne: (table, id) => {
+    return knex(table)
+      .where('uuid', id)
       .first();
   },
-  create(sticker) {
-    return knex('sticker').insert(sticker, '*');
+  create: (table, playlist) => {
+    return knex(table).insert(playlist, '*');
   },
-  update(id, sticker) {
-    return knex('sticker')
+  update: (table, id, playlist) => {
+    return knex(table)
       .where('id', id)
-      .update(sticker, '*');
+      .update(playlist, '*');
   },
-  delete(id) {
-    return knex('sticker')
+  deletePlaylist: (table, id) => {
+    return knex(table)
       .where('id', id)
       .del();
   }
 };
 
-// GET - return all playlists where user_id = user_id
-router.get('/playlists/all', (req, res) => {
-  const id = 'user_id';
-  const getAll = id => {
-    return knex('playlists').where('user_id', id);
-  };
-  getAll().then(playlists => {
+// GET - returns all playlist records where user_uuid = users_pkey
+router.get('/playlists/all/:uuid', (req, res) => {
+  dao.getAll(req.params.id).then(playlists => {
     res.json(playlists);
   });
 });
 
-// GET - return playlist where playlist_id = playlist_id
+// GET - returns all video records where:
+// playlists_pkey = playlist_uuid(playlist_video)
+// AND
+// videos_pkey = video_uuid(playlist_video)
+
+// query the playlist_video table for all records matching playlists_pkey and then
+// parse the result to extract all video_uuid
+// query the vidoes table for all records matching videos_pkey
 router.get('/playlists/:id', (req, res, next) => {
-  const id = 'playlist_id';
-  const getOne = id => {
-    return knex('playlists')
-      .where('id', id)
-      .first();
-  };
-
-  getOne(req.params.id).then(playlist => {
-    if (playlist) {
-      res.json(playlist);
-    } else {
-      next();
-    }
+  dao.getOne(req.params.id).then(playlist => {
+    playlist ? res.json(playlist) : next();
   });
 });
 
-// POST - create a playlist
-// user--playlist--playlist_video--video
+// POST - create a playlist record
 router.post('/playlist', (req, res, next) => {
-  const create = sticker => {
-    return knex('sticker').insert(sticker, '*');
-  };
-
-  create(req.body).then(stickers => {
-    res.json(stickers[0]);
+  dao.create('playlists', req.body).then(playlists => {
+    res.json(playlists[0]);
   });
 });
 
-// PUT - update a playlist
+// POST - create a playlist_video record
+router.post('/playlist_video', (req, res, next) => {
+  dao.create('playlist_video', req.body).then(playlists => {
+    res.json(playlists[0]);
+  });
+});
+
+// PUT - update a playlist record
 router.put('/playlist/:id', (req, res, next) => {
-  const update = (id, sticker) => {
-    return knex('sticker')
-      .where('id', id)
-      .update(sticker, '*');
-  };
-
-  update(req.params.id, req.body).then(stickers => {
-    res.json(stickers[0]);
+  dao.update('playlist', req.params.id, req.body).then(playlists => {
+    res.json(playlists[0]);
   });
 });
 
-// DELETE - delete a playlist
-router.delete('/playlist/:id', (req, res) => {
-  const deletePlaylist = id => {
-    return knex('sticker')
-      .where('id', id)
-      .del();
-  };
+// PUT - update a playlist_video record
+router.put('/playlist_video/:id', (req, res, next) => {
+  dao.update('playlist_video', req.params.id, req.body).then(playlists => {
+    res.json(playlists[0]);
+  });
+});
 
-  deletePlaylist(req.params.id).then(() => {
-    res.json({
-      deleted: true
-    });
+// DELETE - delete playlist record
+router.delete('/playlist/:id', (req, res) => {
+  dao.deletePlaylist('playlists', req.params.id).then(() => {
+    res.json({ deleted: true });
+  });
+});
+
+// DELETE - delete a playlist_video
+router.delete('/playlist_video/:id', (req, res) => {
+  dao.deletePlaylist('playlist_video', req.params.id).then(() => {
+    res.json({ deleted: true });
   });
 });
 
