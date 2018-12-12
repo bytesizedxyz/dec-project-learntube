68ad2d0bef5130fb4e7d48b5831083bd9c17e817
Shalayne Kemp <shaykemp89@gmail.com>
2018-12-12 10:01:38 -0700

    changed default #modal-root display from flex to 'none' so it won't block other elements on the page

--- frontend/src/pages/LandingPage/index.js
+++ frontend/src/pages/LandingPage/index.js
@@ -6,9 +6,7 @@ const index = ({viewVideo}) => {
   return (
     <main>
       <h1>Landing Page</h1>
-      <p>
-      <button onFocus={(e)=>{console.log("buton clicked");viewVideo("QaVXaMFc6gk")}}>watch a video</button>
-      </p>
+      <button onClick={()=>{viewVideo("QaVXaMFc6gk")}}>watch a video</button>
     </main>
   );
 };
