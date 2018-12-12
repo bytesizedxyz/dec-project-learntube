9b19eaf881c77bd5079946ab4424fe99bd459de4
David Kruse <thedavidkruse@gmail.com>
2018-12-12 10:00:56 -0700

    fixed the initialization of the knexfiles

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -2,7 +2,12 @@
 // run this query in your database as it's needed to generate the uuid's for multiple tables:
 // CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+
+// In addition, this is needed in this file so it will read the .env file variables upon running the knex commands.
+
 require('dotenv').config();
+
+// Variables used in the knex commands.
 const connection = {
   host: process.env.DATABASE_HOST,
   user: process.env.DATABASE_USER,
