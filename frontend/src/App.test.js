7bbfd5e4c09aae98577710815344fbdf5644f239
Shalayne Kemp <shaykemp89@gmail.com>
2018-12-12 10:29:42 -0700

    forgot to save after removing unnecessary imports

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
-import store from "./state";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
