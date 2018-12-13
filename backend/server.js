ca9a5c7b4cf32dde488b1911bd2b4fcb061ddf72
Kevin Griego <kev.gri32@gmail.com>
2018-12-13 10:23:42 -0700

    deleted jwt middleware from server file

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ const logger = require('morgan');
 const knex = require('./db/knex');
 const session = require('express-session');
 const passport = require('passport');
-const jwtMiddleware = require('./middleware/jwtMiddleware');
 
 const users = require('./routes/users');
 const videos = require('./routes/videos');
@@ -32,11 +31,8 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/users', users);
-
-// app.use(jwtMiddleware);
-
 //routes
+app.use('/users', users);
 app.use('/videos', videos);
 app.use('/playlists', playlists);
 
