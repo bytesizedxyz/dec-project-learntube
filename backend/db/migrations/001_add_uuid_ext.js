d8915bc97cf0bd84b6168a08f702cc679657e26f
Kristian Freeman <kristian@kristianfreeman.com>
2018-12-13 14:22:10 -0700

    Drop extension on down migration

--- backend/db/migrations/001_add_uuid_ext.js
+++ backend/db/migrations/001_add_uuid_ext.js
@@ -2,3 +2,8 @@ exports.up = (knex, Promise) => {
   const rawSql = 'CREATE EXTENSION IF NOT EXISTS "uuid-ossp";';
   knex.raw(rawSql);
 };
+
+exports.down = (knex, Promise) => {
+  const rawSql = 'DROP EXTENSION IF EXISTS "uuid-ossp";';
+  return knex.raw(rawSql);
+};
