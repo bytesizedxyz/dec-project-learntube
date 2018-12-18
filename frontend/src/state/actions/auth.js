4a2b7acc1652f201d66af59358d78120e6d303fb
David Kruse <thedavidkruse@gmail.com>
2018-12-18 12:52:03 -0700

    gottem

--- frontend/src/state/actions/auth.js
+++ frontend/src/state/actions/auth.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import api from '../apis/backend';
 /*
  * action types
  */
@@ -11,35 +12,60 @@ export const LOGIN = "LOGIN";
 export const signup = () => (dispatch, getState) => {
   // axios request to signup
 
-  const payload = { logged_in: true };
+  const payload = {
+    logged_in: true
+  };
   // data for redux store is a boolean flag
-  dispatch({ type: SIGNUP, payload });
+  dispatch({
+    type: SIGNUP,
+    payload
+  });
 };
 
 export const login = (username, password) => async (dispatch, getState) => {
-      console.log(username, password)
-      // axios request to login
-      //console.log("THE DISPATCH: ", dispatch);
-      // const blah = await axios.post("https://dry-river-42897.herokuapp.com/users/sign_in")
-      const blah = await  Promise.resolve({"data":{"user":{"username":"mmmm","email":"mmmm","is_admin":null},"token":"8"}})
-      const { token, user } = blah.data
-      
-        localStorage.setItem("token", token)
-        const payload = { 
-          logged_in: true,
-          username: user.username,
-          email: user.email
-        };
-        dispatch({ type: LOGIN, payload }); // data for redux store is a boolean flag
-  
+  console.log(username, password)
+  // axios request to login
+  //console.log("THE DISPATCH: ", dispatch);
+  // const blah = await api.post("/users/sign_in",{username, password})
+  const blah = await Promise.resolve({
+    "data": {
+      "user": {
+        "username": "mmmm",
+        "email": "mmmm",
+        "is_admin": null
+      },
+      "token": "8"
+    }
+  })
+  const {
+    token,
+    user
+  } = blah.data
+
+  localStorage.setItem("token", token)
+  const payload = {
+    logged_in: true,
+    username: user.username,
+    email: user.email
+  };
+  dispatch({
+    type: LOGIN,
+    payload
+  }); // data for redux store is a boolean flag
+
 };
 
 export const logout = () => async (dispatch, getState) => {
   // axios request to logout
   localStorage.removeItem("token");
-  const payload = { logged_in: false };
+  const payload = {
+    logged_in: false
+  };
   // data for redux store is a boolean flag
-  dispatch({ type: LOGOUT, payload });
+  dispatch({
+    type: LOGOUT,
+    payload
+  });
 };
 
 //TODO:
